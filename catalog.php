17db8957f6da5e077885771c49810d7b54a7effb
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2016-09-25 13:48:24 -0700

    limiting records in sql, specific queries and filtering

---
+++ catalog.php
@@ -0,0 +1,47 @@
+<?php 
+include("inc/functions.php");
+
+$pageTitle = "Full Catalog";
+$section = null;
+
+if (isset($_GET["cat"])) {
+    if ($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+        $section = "books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+        $section = "movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+        $section = "music";
+    }
+}
+if (empty($section)) {
+    $catalog = full_catalog_array();
+} else {
+    $catalog = category_catalog_array($section);
+}
+include("inc/header.php"); ?>
+
+<div class="section catalog page">
+    
+    <div class="wrapper">
+        
+        <h1><?php 
+        if ($section != null) {
+            echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
+        }
+        echo $pageTitle; ?></h1>
+        
+        <ul class="items">
+            <?php
+            foreach ($catalog as $item) {
+                echo get_item_html($item);
+            }
+            ?>
+        </ul>
+        
+    </div>
+</div>
+
+<?php include("inc/footer.php"); ?>
