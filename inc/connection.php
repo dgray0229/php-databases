d5b3729bd29152ad8c4fecadbd99383abac24d6a
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2017-01-02 08:59:49 -0800

    connection to remote database

--- inc/connection.php
+++ inc/connection.php
@@ -1,8 +1,20 @@
 <?php
 
 try {
-  $db = new PDO("sqlite:".__DIR__."/database.db");
-  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $URL = getenv('JAWSDB_URL');
+    var_dump($URL);
+    $dbparts = parse_url($HOST);
+
+    $HOST = $dbparts['host'];
+    $USER = $dbparts['user'];
+    $PASS = $dbparts['pass'];
+    $DATABASE = ltrim($dbparts['path'], '/');
+
+    $db = new PDO(
+        "mysql:host=$HOST;
+        dbname=$DATABASE",
+        $USER, $PASS
+    );
 } catch (Exception $e) {
   echo "Unable to connect";
   //echo $e->getMessage();
