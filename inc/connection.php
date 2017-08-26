dcaff3787b939a0a54c608e69ff566474a0356e5
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2017-08-25 22:52:43 -0700

    migrated database from local sql db to MySQL db. Preparing for Heroku Deploy

--- inc/connection.php
+++ inc/connection.php
@@ -1,23 +1,13 @@
 <?php
 
 try {
-    $URL = getenv('JAWSDB_URL');
-    $dbparts = parse_url($HOST);
-
-    $HOST = $dbparts['host'];
-    $USER = $dbparts['user'];
-    $PASS = $dbparts['pass'];
-    $DATABASE = ltrim($dbparts['path'], '/');
-    $OPTIONS = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-    );
-
-    $db = new PDO(
-        "mysql:host=$HOST;
-        dbname=$DATABASE",
-        $USER, $PASS, $OPTIONS
-    );
-} catch (Exception $e) {
+//  $db = new PDO("sqlite:".__DIR__."/database.db");
+    $server = getenv('JAWSDB_URL');
+    $user = getenv('USERNAME');
+    $pass = getenv('PASSWORD');
+    $db = new PDO($server, $user, $pass);
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
   echo "Unable to connect";
   //echo $e->getMessage();
   exit;
